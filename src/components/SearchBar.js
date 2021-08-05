eea8439cfb3568aa1ae7d12176f661054e3a216d
Anton <enitefall81@gmail.com>
2021-08-05 12:03:01 -0700

    onChange refactor into a separate method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,21 @@ import React from "react"
 
 class SearchBar extends React.Component {
 state = {search: ''}
-
+onChange = (event) => {
+  this.setState({search: event.target.value})
+}
   onFormSubmit = (event) => {
 
   }
 
   render() {
 
-    return <div>
-      <form>
+    return <div className="search-bar ui segment">
+      <form className="ui form">
+        <div className="field">
         <label>Search for videos here</label>
-        <input onChange={e => this.setState({search: e.target.value})}/>
+        <input type="text" onChange={this.onChange}/>
+         </div>
       </form>
       {console.log(this.state)}
      </div>
