3fb46136275d0e2d2aaac9d0e21e67f7b43dc04d
Anton <enitefall81@gmail.com>
2021-08-09 09:45:27 -0700

    Add VideoItem styles

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,12 @@
 import React from "react"
 
 let VideoItem = ({video}) => {
- return <div>
-  <img src={video.snippet.thumbnails.default.url}/>
-  {video.snippet.title}
+ return <div className="item">
+  <img className="ui image"
+       src={video.snippet.thumbnails.medium.url}/>
+  <div className="content">
+  <div className="header"> {video.snippet.title} </div>
+   </div>
  </div>
 }
 
--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -4,9 +4,9 @@ import VideoItem from "./VideoItem";
 let videosList = ({videos}) => {
   console.log(videos)
    let renderedList = videos.map((video) => {
-     return <VideoItem video={video}/>
+     return <VideoItem video={video} id={video.id.videoId}/>
    })
-  return <div>
+  return <div className="ui relaxed divided list">
     {renderedList}
   </div>
 }
