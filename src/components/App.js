82424e46e9a522c6e46c5b38b4df7ce03be4164a
Anton Durandin <enitefall81@gmail.com>
2021-08-21 12:57:50 -0700

    Remove context

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react"
+import React  from "react"
 import SearchBar from "./SearchBar"
 import youtube from "../apis/youtube"
 import VideosList from "./VideosList";
