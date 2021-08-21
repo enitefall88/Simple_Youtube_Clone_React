7b266a39c1c5b557fe1ff9b52690f5c6666fe449
Anton Durandin <enitefall81@gmail.com>
2021-08-21 12:41:16 -0700

    Fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useContext} from "react"
 import SearchBar from "./SearchBar"
 import youtube from "../apis/youtube"
 import VideosList from "./VideosList";
