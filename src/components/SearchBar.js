886c6cbb4d9a32ead93fd930040474c31fa0e1bb
Anton Durandin <enitefall81@gmail.com>
2021-09-28 13:08:29 -0700

    Remove the old searchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,28 +18,4 @@ let SearchBar = ({onSearchSubmit}) => {
      </div>
 }
 
-class SearchBarOld extends React.Component {
-state = {search: ''}
-onChange = (event) => {
-  this.setState({search: event.target.value})
-}
-  onFormSubmit = (event) => {
-  event.preventDefault()
-  this.props.onSearchSubmit(this.state.search)
-  }
-
-  render() {
-
-    return <div className="search-bar ui segment">
-      <form onSubmit={this.onFormSubmit} className="ui form">
-        <div className="field">
-        <label>Search for videos here</label>
-        <input type="text" onChange={this.onChange}/>
-         </div>
-      </form>
-      {console.log(this.state)}
-     </div>
-  }
-}
-
 export default SearchBar
