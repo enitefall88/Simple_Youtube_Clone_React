e9913bcd8d9b950a8b8519551469a75c87729d69
Anton Durandin <enitefall81@gmail.com>
2021-09-28 13:37:31 -0700

    Refactor App with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,52 @@
-import React  from "react"
+import React, {useEffect, useState} from "react"
 import SearchBar from "./SearchBar"
 import youtube from "../apis/youtube"
 import VideosList from "./VideosList";
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component {
+function App () {
+  let [videos, setVideos] = useState([])
+  let [selectedVideo, setSelectedVideo] = useState(null)
+
+  let onSearchSubmit = async searchString => {
+    let response = await youtube.get('/search', {
+      params: {
+        q: searchString
+      }
+    })
+    setVideos(response.data.items)
+    setSelectedVideo(response.data.items[0])
+    }
+    //console.log(this.state)
+
+    let onVideoSelect = (video) => {
+    console.log('From the app', video)
+    setSelectedVideo(video)
+  }
+
+  useEffect(() => {
+    onSearchSubmit('swords')
+  }, [])
+
+   return <div>
+      <SearchBar onSearchSubmit={onSearchSubmit}/>
+      <div className="ui grid">
+        <div className="ui row">
+        <div className="eleven wide column">
+          <VideoDetail video={selectedVideo}/>
+          </div>
+        <div className="five wide column">
+          <VideosList videos={videos}
+                    onVideoSelect={onVideoSelect}
+        />
+        </div>
+        </div>
+      </div>
+
+    </div>
+}
+
+class AppOld extends React.Component {
   state = {videos: [], selectedVideo: null}
 
   componentDidMount() {
